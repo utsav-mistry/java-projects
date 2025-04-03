64a6f33c2b04b7f67bb0d9316a663f666360fb5f
Utsav Mistry <143418671+utsav-mistry@users.noreply.github.com>
2025-04-03 21:38:21 +0530

    Delete hospital directory

--- hospital/src/credentials.java
+++
@@ -1,8 +0,0 @@
-public class credentials {
-    public static String nameAdmin = "admin";
-    public static String passAdmin = "hospital.admin";
-    public static String nameStaff = "staff";
-    public static String passStaff = "hospital.staff";
-    public static String nameReception = "reception";
-    public static String passReception = "hospital.reception";
-}
